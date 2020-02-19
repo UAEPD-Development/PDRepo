43b47a4a0467881fe0e958eb0e3008c685030a76
ahmedhashem <ahmedhashem.ah988@gmaol.com>
2020-02-19 13:16:25 +0400

    Create Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'; 
+import {route} from './router/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './admin/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/router/router.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+
+
+
+export const route:Routes = [
+    // {path: '', redirectTo:'', pathMatch: 'fill'}
+]
